e0c6191561c2e0eb05cdfe1eb5f88c4f071257ac
Liz Couture <liz.a.couture@gmail.com>
2020-04-11 17:32:37 -0400

    Set up play place
    Added a bunch of cards too

---
+++ Assets/Scripts/PlayerTag.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class PlayerTag : MonoBehaviour
+{
+    public string playerName = "playerName";
+    public Sprite playerAvatar;
+    public TextMeshProUGUI playerLabel;
+    public Image avatarFrame;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      if (playerName != null && playerLabel != null)
+        {
+            playerLabel.text = playerName;
+        } 
+      if (playerAvatar != null && avatarFrame != null)
+        {
+            avatarFrame.sprite = playerAvatar;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

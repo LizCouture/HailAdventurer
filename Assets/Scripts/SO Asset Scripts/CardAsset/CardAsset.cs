af17780c75ccff31a5a4bdf2b348373864dba6d3
Liz Couture <liz.a.couture@gmail.com>
2020-04-10 22:12:13 -0400

    Created More Items
    So many items!

---
+++ Assets/Scripts/SO Asset Scripts/CardAsset/CardAsset.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CardAsset : ScriptableObject 
+{
+    // this object will hold the info about the most general card
+    [Header("General info")]
+
+    [TextArea(2,3)]
+    public string Description;  // Description for spell or character
+	public Sprite CardImage;
+
+    [Header("Item Info")]
+    public string ItemScriptName;
+
+    [Header("Adventurer Info")]
+    public string AdventurerScriptName;
+
+
+
+}
---
+++ Assets/Scripts/Visual/OneCardManager.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using TMPro;
+
+// holds the refs to all the Text, Images on the card
+public class OneCardManager : MonoBehaviour {
+
+    public CardAsset cardAsset;
+    public OneCardManager PreviewManager;
+    [Header("Text Component References")]
+    public TextMeshProUGUI DescriptionText;
+
+    [Header("Image References")]
+    public Image CardFaceFrameImage;
+    public Image CardGraphicImage;
+    public Image CardBodyImage;
+    public Image CardBackImage;
+    public Image CardFaceGlowImage;
+    public Image CardBackGlowImage;
+
+    void Awake()
+    {
+        if (cardAsset != null)
+            ReadCardFromAsset();
+    }
+
+    private bool canBePlayedNow = false;
+    public bool CanBePlayedNow
+    {
+        get
+        {
+            return canBePlayedNow;
+        }
+
+        set
+        {
+            canBePlayedNow = value;
+
+            CardFaceGlowImage.enabled = value;
+        }
+    }
+
+    public void ReadCardFromAsset()
+    {
+        // universal actions for any Card
+        // 1) apply tint
+            //CardBodyImage.color = GlobalSettings.Instance.CardBodyStandardColor;
+            CardFaceFrameImage.color = Color.white;
+            //CardTopRibbonImage.color = GlobalSettings.Instance.CardRibbonsStandardColor;
+            //CardLowRibbonImage.color = GlobalSettings.Instance.CardRibbonsStandardColor;
+          
+        // 4) add description
+        DescriptionText.text = cardAsset.Description;
+        // 5) Change the card graphic sprite
+        CardGraphicImage.sprite = cardAsset.CardImage;
+
+       
+        if (PreviewManager != null)
+        {
+            // this is a card and not a preview
+            // Preview GameObject will have OneCardManager as well, but PreviewManager should be null there
+            PreviewManager.cardAsset = cardAsset;
+            PreviewManager.ReadCardFromAsset();
+        }
+    }
+}
